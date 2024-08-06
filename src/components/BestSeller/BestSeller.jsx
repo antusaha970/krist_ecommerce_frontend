8fb3ee9c53f698235f7cf669b2979c1fc3b2a033
Antu Saha <antusaha970@gmail.com>
2024-08-06 14:03:29 +0600

    added best seller

--- src/components/BestSeller/BestSeller.jsx
+++ src/components/BestSeller/BestSeller.jsx
@@ -20,6 +20,62 @@ const BestSeller = () => {
   return (
     <section className="container my-5">
       <h2 className="text-center fw-bold">Our Bestseller</h2>
+      <div className="container">
+        <div className="row">
+          {products?.map((product) => (
+            <div className="col-md-3 col-sm-6 col-12" key={product.id}>
+              <div className="product-grid">
+                <div className="product-image">
+                  <a href="#" className="image">
+                    <img
+                      className="pic-1"
+                      src={
+                        product.product_images.length > 0
+                          ? product.product_images[0].images
+                          : ""
+                      }
+                    />
+                    <img
+                      className="pic-2"
+                      src={
+                        product.product_images.length > 1
+                          ? product.product_images[1].images
+                          : ""
+                      }
+                    />
+                  </a>
+                  <span className="product-sale-label">hot</span>
+                </div>
+                <div className="product-content">
+                  <ul className="rating">
+                    <li className="fas fa-star" />
+                    <li className="fas fa-star" />
+                    <li className="fas fa-star" />
+                    <li className="fas fa-star" />
+                    <li className="fas fa-star disable" />
+                  </ul>
+                  <h3 className="title">
+                    <a href="#"> {product.title} </a>
+                  </h3>
+                  <div className="price">${product.price} </div>
+                  <div className="product-button-group">
+                    <a className="product-like-icon" href="#">
+                      <i className="fas fa-heart" />
+                    </a>
+                    <div className="add-to-cart" href="#">
+                      <i className="fa fa-shopping-bag" />
+                      ADD TO CART
+                    </div>
+                    <a className="product-compare-icon" href="#">
+                      <i className="fas fa-random" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
