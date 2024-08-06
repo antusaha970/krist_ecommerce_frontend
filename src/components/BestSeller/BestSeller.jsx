e817044cb9bf246ea29f99204abb8d649dbe3fc3
Antu Saha <antusaha970@gmail.com>
2024-08-06 14:18:34 +0600

    fixed bugs and space

--- src/components/BestSeller/BestSeller.jsx
+++ src/components/BestSeller/BestSeller.jsx
@@ -19,7 +19,7 @@ const BestSeller = () => {
 
   return (
     <section className="container m_top_bottom">
-      <h2 className="text-center fw-bold">Our Bestseller</h2>
+      <h2 className="text-center fw-bold mb-5">Our Bestseller</h2>
       <div className="container">
         <div className="row">
           {products?.map((product) => (
