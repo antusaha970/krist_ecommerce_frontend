f108f7a3e86a2998faee8a60f3a51572654caa57
Antu Saha <antusaha970@gmail.com>
2024-08-08 00:58:42 +0600

    added description and additional description and reviews

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -35,6 +35,7 @@ const ProductDetails = () => {
 
   return (
     <div className="container">
+      {/* product card */}
       <div className="card">
         <div className="container-fliud">
           <div className="wrapper row">
@@ -108,6 +109,78 @@ const ProductDetails = () => {
           </div>
         </div>
       </div>
+      {/* product card */}
+      {/* product additional information */}
+      <div className="m_top_bottom">
+        <div>
+          <div className="nav nav-tabs" id="nav-tab" role="tablist">
+            <button
+              className="nav-link active"
+              id="nav-description-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-description"
+              type="button"
+              role="tab"
+              aria-controls="nav-description"
+              aria-selected="true"
+            >
+              Description
+            </button>
+            <button
+              className="nav-link"
+              id="nav-info-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-info"
+              type="button"
+              role="tab"
+              aria-controls="nav-info"
+              aria-selected="false"
+            >
+              Additional information
+            </button>
+            <button
+              className="nav-link"
+              id="nav-contact-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-contact"
+              type="button"
+              role="tab"
+              aria-controls="nav-contact"
+              aria-selected="false"
+            >
+              Reviews
+            </button>
+          </div>
+        </div>
+        <div className="tab-content" id="nav-tabContent">
+          <div
+            className="tab-pane fade show active"
+            id="nav-description"
+            role="tabpanel"
+            aria-labelledby="nav-description-tab"
+          >
+            <p className="text_18">{product?.description}</p>
+          </div>
+          <div
+            className="tab-pane fade"
+            id="nav-info"
+            role="tabpanel"
+            aria-labelledby="nav-info-tab"
+          >
+            <p className="text_18">{product?.additional_info}</p>
+          </div>
+          <div
+            className="tab-pane fade"
+            id="nav-contact"
+            role="tabpanel"
+            aria-labelledby="nav-contact-tab"
+          >
+            else 2
+          </div>
+        </div>
+      </div>
+
+      {/* product additional information */}
     </div>
   );
 };
