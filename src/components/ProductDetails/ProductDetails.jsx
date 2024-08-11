a6b8622e87ca7fbefc442ea6254f1458b49aedf3
Antu Saha <antusaha970@gmail.com>
2024-08-12 02:06:34 +0600

    added loader on product details page

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -7,6 +7,7 @@ import ProductAdditionalInformation from "../ProductAdditionalInformation/Produc
 import RelatedProduct from "../RelatedProduct/RelatedProduct";
 import OurService from "../Shared/OurService/OurService";
 import { toast } from "react-toastify";
+import Loader from "../Shared/Loader/Loader";
 const settings = {
   dots: true,
   infinite: true,
@@ -51,86 +52,97 @@ const ProductDetails = () => {
     }
   };
 
+  const handleAddToCart = async () => {};
+
   return (
     <div className="container">
+      {/* loader */}
+      {isLoading && <Loader />}
+      {/* loader */}
       {/* product card */}
-      <div className="card">
-        <div className="container-fliud">
-          <div className="wrapper row">
-            {/* product images box */}
-            <div className="preview col-md-6 col-sm-12 col-12">
-              <div className="slider-container">
-                <Slider {...settings}>
-                  {product?.product_images?.map((pd_img, idx) => (
-                    <div key={idx}>
-                      <img src={pd_img.images} alt="" className="img-fluid" />
-                    </div>
+      {!isLoading && (
+        <div className="card">
+          <div className="container-fliud">
+            <div className="wrapper row">
+              {/* product images box */}
+              <div className="preview col-md-6 col-sm-12 col-12">
+                <div className="slider-container">
+                  <Slider {...settings}>
+                    {product?.product_images?.map((pd_img, idx) => (
+                      <div key={idx}>
+                        <img src={pd_img.images} alt="" className="img-fluid" />
+                      </div>
+                    ))}
+                  </Slider>
+                </div>
+              </div>
+              {/* product images box */}
+              {/* product details box */}
+              <div className="details col-md-6 col-sm-12 col-12">
+                <h3 className="product-title"> {product?.title} </h3>
+                <div className="rating">
+                  <span className="review-no">
+                    Rating: {product?.rating}{" "}
+                    <span className="fa fa-star checked" />
+                  </span>
+                </div>
+                <p className="product-description">{product?.description}</p>
+                <h4 className="price">
+                  current price: <span>${product?.price}</span>
+                </h4>
+                <p className="vote">
+                  Category: {product?.categories?.join(", ")}
+                </p>
+                <h5 className="sizes">
+                  sizes:
+                  {product?.sizes?.map((size) => (
+                    <span
+                      key={size}
+                      className="size"
+                      data-toggle="tooltip"
+                      title={size}
+                    >
+                      {size}
+                    </span>
                   ))}
-                </Slider>
-              </div>
-            </div>
-            {/* product images box */}
-            {/* product details box */}
-            <div className="details col-md-6 col-sm-12 col-12">
-              <h3 className="product-title"> {product?.title} </h3>
-              <div className="rating">
-                <span className="review-no">
-                  Rating: {product?.rating}{" "}
-                  <span className="fa fa-star checked" />
-                </span>
-              </div>
-              <p className="product-description">{product?.description}</p>
-              <h4 className="price">
-                current price: <span>${product?.price}</span>
-              </h4>
-              <p className="vote">
-                Category: {product?.categories?.join(", ")}
-              </p>
-              <h5 className="sizes">
-                sizes:
-                {product?.sizes?.map((size) => (
-                  <span
-                    key={size}
-                    className="size"
-                    data-toggle="tooltip"
-                    title={size}
+                </h5>
+                <h5 className="colors">
+                  colors:
+                  {product?.colors?.map((color) => (
+                    <span
+                      key={color}
+                      className="size"
+                      data-toggle="tooltip"
+                      title={color}
+                    >
+                      {color}
+                    </span>
+                  ))}
+                </h5>
+                <h5 className="sizes">In stock : {product?.stock} </h5>
+                <div className="action">
+                  <button
+                    className="base_button_2 me-2"
+                    type="button"
+                    onClick={handleAddToCart}
                   >
-                    {size}
-                  </span>
-                ))}
-              </h5>
-              <h5 className="colors">
-                colors:
-                {product?.colors?.map((color) => (
-                  <span
-                    key={color}
-                    className="size"
-                    data-toggle="tooltip"
-                    title={color}
-                  >
-                    {color}
-                  </span>
-                ))}
-              </h5>
-              <h5 className="sizes">In stock : {product?.stock} </h5>
-              <div className="action">
-                <button className="base_button_2 me-2" type="button">
-                  Add to cart <i className="fa-solid fa-cart-shopping"></i>
-                </button>
+                    Add to cart <i className="fa-solid fa-cart-shopping"></i>
+                  </button>
 
-                <button
-                  className="base_button"
-                  type="button"
-                  onClick={handleAddToWishList}
-                >
-                  <span className="fa fa-heart" />
-                </button>
+                  <button
+                    className="base_button"
+                    type="button"
+                    onClick={handleAddToWishList}
+                  >
+                    <span className="fa fa-heart" />
+                  </button>
+                </div>
               </div>
+              {/* product details box */}
             </div>
-            {/* product details box */}
           </div>
         </div>
-      </div>
+      )}
       {/* product card */}
 
       {/* product additional information */}
