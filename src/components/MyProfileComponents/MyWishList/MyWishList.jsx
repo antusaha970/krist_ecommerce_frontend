d809f973dac7144edddbd69e21f2a4514df900b4
Antu Saha <antusaha970@gmail.com>
2024-09-16 22:25:37 +0600

    added loader to wishlist component

--- src/components/MyProfileComponents/MyWishList/MyWishList.jsx
+++ src/components/MyProfileComponents/MyWishList/MyWishList.jsx
@@ -2,21 +2,25 @@ import { useEffect, useState } from "react";
 import ProfileNavigation from "../ProfileNavigation/ProfileNavigation";
 import client from "../../../api_client/api_client";
 import ProductCard from "../../Shared/ProductCard/ProductCard";
+import Loader from "../../Shared/Loader/Loader";
 
 const MyWishList = () => {
   const [wishlist, setWishlist] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const getWishListItems = async () => {
       try {
+        setIsLoading(true);
         const response = await client.get("/api/wishlist/");
         setWishlist(response.data);
       } catch (error) {
         console.error(error);
+      } finally {
+        setIsLoading(false);
       }
     };
     getWishListItems();
   }, []);
-  console.log(wishlist);
 
   return (
     <section className="container m_top_bottom">
@@ -26,6 +30,10 @@ const MyWishList = () => {
           <ProfileNavigation />
         </div>
         <div className="col-12 col-sm-6 col-md-8">
+          {isLoading && <Loader />}
+          {!isLoading && wishlist.length == 0 && (
+            <h3>You have no item in wishlist</h3>
+          )}
           <div className="row">
             {wishlist?.map((product) => (
               <ProductCard product={product.products} key={product.id} />
