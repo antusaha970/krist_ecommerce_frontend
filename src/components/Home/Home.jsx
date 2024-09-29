9a6e6e6636a966046cf54f12d5f2734f54d6029f
Antu Saha <antusaha970@gmail.com>
2024-09-29 20:08:34 +0600

    added new banner

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import BestSeller from "../BestSeller/BestSeller";
 import DealsCounter from "../DealsCounter/DealsCounter";
 import Hero from "../Hero/Hero";
 import InstaStory from "../InstaStory/InstaStory";
+import LandingBanner from "../LandingBanner/LandingBanner";
 import OurFeatures from "../OurFeatures/OurFeatures";
 import ShopByCategorySlider from "../ShopByCategorySlider/ShopByCategorySlider";
 import Testimonial from "../Testimonial/Testimonial";
@@ -9,6 +10,7 @@ import Testimonial from "../Testimonial/Testimonial";
 const Home = () => {
   return (
     <>
+      <LandingBanner />
       <Hero />
       <ShopByCategorySlider />
       <BestSeller />
