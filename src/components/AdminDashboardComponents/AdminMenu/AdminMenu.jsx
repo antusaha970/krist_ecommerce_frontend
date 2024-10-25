d69344f333e0ccdeda9972de0636d01d83802ebe
Antu Saha <antusaha970@gmail.com>
2024-10-25 20:17:49 +0600

    added overview page in admin dashboard

--- src/components/AdminDashboardComponents/AdminMenu/AdminMenu.jsx
+++ src/components/AdminDashboardComponents/AdminMenu/AdminMenu.jsx
@@ -1,4 +1,5 @@
 import AdminDashBoardMenu from "../AdminDashBoardMenu/AdminDashBoardMenu";
+import "./AdminMenu.css";
 const AdminMenu = () => {
   return (
     <section className="container-fluid my-5">
@@ -6,7 +7,79 @@ const AdminMenu = () => {
         <div className="col-md-2 col-sm-12 col-12 shadow-sm ps-4 text-center">
           <AdminDashBoardMenu />
         </div>
-        <div className="col-md-10 col-sm-12 col-12">Overview</div>
+        <div className="col-md-10 col-sm-12 col-12">
+          <h2 className="mb-0 text-center">Overview</h2>
+          <div className="row ">
+            <div className="col-xl-6 col-lg-6">
+              <div className="card l-bg-cherry">
+                <div className="card-statistic-3 p-4">
+                  <div className="card-icon card-icon-large">
+                    <i className="fas fa-shopping-cart" />
+                  </div>
+                  <div className="mb-4">
+                    <h5 className="card-title mb-0">New Orders</h5>
+                  </div>
+                  <div className="row align-items-center mb-2 d-flex">
+                    <div className="col-8">
+                      <h2 className="d-flex align-items-center mb-0">3,243</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6">
+              <div className="card l-bg-blue-dark">
+                <div className="card-statistic-3 p-4">
+                  <div className="card-icon card-icon-large">
+                    <i className="fas fa-users" />
+                  </div>
+                  <div className="mb-4">
+                    <h5 className="card-title mb-0">Total customer</h5>
+                  </div>
+                  <div className="row align-items-center mb-2 d-flex">
+                    <div className="col-8">
+                      <h2 className="d-flex align-items-center mb-0">15</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6">
+              <div className="card l-bg-green-dark">
+                <div className="card-statistic-3 p-4">
+                  <div className="card-icon card-icon-large">
+                    <i className="fas fa-ticket-alt" />
+                  </div>
+                  <div className="mb-4">
+                    <h5 className="card-title mb-0">Total amount sell</h5>
+                  </div>
+                  <div className="row align-items-center mb-2 d-flex">
+                    <div className="col-8">
+                      <h2 className="d-flex align-items-center mb-0">578</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6">
+              <div className="card l-bg-orange-dark">
+                <div className="card-statistic-3 p-4">
+                  <div className="card-icon card-icon-large">
+                    <i className="fas fa-dollar-sign" />
+                  </div>
+                  <div className="mb-4">
+                    <h5 className="card-title mb-0">Revenue Today</h5>
+                  </div>
+                  <div className="row align-items-center mb-2 d-flex">
+                    <div className="col-8">
+                      <h2 className="d-flex align-items-center mb-0">$0</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
